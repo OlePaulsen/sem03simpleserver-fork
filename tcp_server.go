34546c936173fc651beedd188b3de20a801f91ec
root <olepaulsen@outlook.com>
2023-04-17 16:36:52 +0000

    1

--- tcp_server.go
+++ tcp_server.go
@@ -5,7 +5,56 @@ import (
 	"log"
 	"net"
 	"sync"
+	"strings"
+	"strconv"
 )
+
+var ALF_SEM03 []rune = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789.,:; ")
+
+func CelsiusToFahrenheit(value float64) float64 {
+	var fahrenheit float64
+	fahrenheit = (value * 1.8) + 32
+	return fahrenheit
+}
+
+func checkKjevik(dekryptertMelding []rune) []rune {
+    checkKjevik := string(dekryptertMelding)
+    if strings.HasPrefix(checkKjevik, "Kjevik") {
+        runeKjevik := []rune(checkKjevik)
+        lastRune := runeKjevik[len(runeKjevik)-1]
+        lastRuneValue, err := strconv.Atoi(string(lastRune))
+        if err != nil {
+            return dekryptertMelding
+        }
+        newRuneValue := CelsiusToFahrenheit(float64(lastRuneValue))
+        newRuneString := strconv.FormatFloat(newRuneValue, 'f', -1, 64)
+        newRunes := []rune(newRuneString)
+        runeKjevik = append(runeKjevik[:len(runeKjevik)-1], newRunes...)
+        kryptertMelding2 := Krypter(runeKjevik, ALF_SEM03, 4)
+        return kryptertMelding2
+    }
+    return dekryptertMelding
+}
+
+func Krypter(melding []rune, alphabet []rune, chiffer int) []rune {
+        kryptertMelding := make([]rune, len(melding))
+        for i := 0; i < len(melding); i++ {
+                indeks := sokIAlfabetet(melding[i], alphabet)
+                newIndex := (indeks + chiffer) % len(alphabet)
+                kryptertMelding[i] = alphabet[newIndex]
+        }
+        return kryptertMelding
+}
+
+func sokIAlfabetet(symbol rune, alfabet []rune) int {
+	for i := 0; i < len(alfabet); i++ {
+			if symbol == alfabet[i] {
+					return i
+					break
+			}
+	}
+	return -1
+}
 
 func main() {
 
@@ -36,21 +85,29 @@ func main() {
 						}
 						return // fra for løkke
 					}
-					switch msg := string(buf[:n]); msg {
-  				        case "ping":
-						_, err = c.Write([]byte("pong"))
-					default:
-						_, err = c.Write(buf[:n])
-					}
-					if err != nil {
-						if err != io.EOF {
-							log.Println(err)
-						}
-						return // fra for løkke
-					}
-				}
-			}(conn)
-		}
-	}()
-	wg.Wait()
+					dekryptertMelding := Krypter([]rune(string(buf[:n])), ALF_SEM03, len(ALF_SEM03)-4)
+					log.Printf("dekrypt: %s", string(dekryptertMelding))
+					kryptertMelding := Krypter(([]rune("pong")), ALF_SEM03, 4)
+					log.Printf("mld1: %s", string(kryptertMelding))
+					kryptertMelding2 := checkKjevik(dekryptertMelding)
+					log.Printf("mld2: %s", string(kryptertMelding2))	
+                                        switch msg := string(dekryptertMelding); msg {
+                                        case "ping":
+                                                _, err = c.Write([]byte(string(kryptertMelding)))
+					case "Kjevik;SN39040;18.03.2022 01:50;6":
+                                                _, err = c.Write([]byte(string(kryptertMelding2)))
+                                        default:
+                                                _, err = c.Write([]byte(string(dekryptertMelding)))
+                                        }
+                                        if err != nil {
+                                                if err != io.EOF {
+                                                        log.Println(err)
+                                                }
+                                                return // fra for l  kke
+                                        }
+                                }
+                        }(conn)
+                }
+        }()
+        wg.Wait()
 }
